bad85c421108ac8dd3e5341350d2737566791f23
tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2017-11-30 17:24:19 +1300

    added basic events

--- panther/__init__.py
+++ panther/__init__.py
@@ -1,11 +1,12 @@
 """
 Root of the Panther UI module
 """
+from collections import defaultdict
 
 
 class EventManager:
     def __init__(self):
-        self.events = {}
+        self.events = defaultdict(default_factory=lambda: lambda: None)
 
     def add(self, name, function):
         self.events[name] = function
@@ -26,7 +27,17 @@ class PantherApp:
 
         self._eventmanager = EventManager()
 
-    def on(self, event):
+    def add_event_handler(self, event, function, **options):
+        """
+        Adds an event handler
+        :param event: string, name of event
+        :param function: function
+        :param options: dict<extra options>
+        :return: None
+        """
+        self._eventmanager.add(event, function)
+
+    def on(self, event, **options):
         """
         Register an event function using this as a function decorator.
         e.g:
@@ -36,3 +47,34 @@ class PantherApp:
         :param event: string, name of event
         :return:
         """
+
+        def decorator(f):
+            ev = event
+            self.add_event_handler(event, f, **options)
+            return f
+
+        return decorator
+
+    def start(self):
+        """
+        Runs the app
+        :return: None
+        """
+        # start the app up
+
+        # run init
+        self._eventmanager.trigger('init')
+
+
+if __name__ == "__main__":
+    app = PantherApp(
+        "test app",
+        500,
+        500
+    )
+
+    @app.on('init')
+    def panther_init():
+        print("initialising panther app")
+
+    app.start()
